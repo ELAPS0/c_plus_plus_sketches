523e5ce48815e6537b4f5639057a41317d0240e6
bob <elapso@yandex.ru>
2021-12-16 23:43:24 +0300

    correct exmple

--- deleter.cpp
+++ deleter.cpp
@@ -32,11 +32,12 @@ public:
 void testUnique() {
     std::cout << "=== testUnique begin ===" << std::endl;
 
-    auto derived = std::make_unique<Derived>(1);
-    derived->sayHello();
+    //auto derived = std::make_unique<Derived>(1);
+    std::unique_ptr<Base> b0 = std::make_unique<Derived>(1);
+    b0->sayHello();
 
-    std::unique_ptr<Base> base = std::move(derived);
-    base->sayHello();
+    std::unique_ptr<Base> b1 = std::move(b0);
+    b1->sayHello();
 
 
     std::cout << "=== testUnique end ===" << std::endl;
@@ -45,11 +46,12 @@ void testUnique() {
 void testShared() {
     std::cout << "=== testShared begin ===" << std::endl;
 
-    auto derived = std::make_shared<Derived>(1);
-    derived->sayHello();
+    //auto derived = std::make_shared<Derived>(1);
+    std::shared_ptr<Base> b = std::make_shared<Derived>(1);
+    b->sayHello();
 
-    auto base = std::static_pointer_cast<Base>(derived);
-    base->sayHello();
+    //auto base = std::static_pointer_cast<Base>(derived);
+    //base->sayHello();
 
 
     std::cout << "=== testShared end ===" << std::endl;
