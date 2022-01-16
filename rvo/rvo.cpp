0c76374d5f0c01c04cc5f1941d90773abb72276b
bob <elapso@yandex.ru>
2022-01-16 19:28:20 +0300

    move semantic return optimisation example [+]

--- rvo/rvo.cpp
+++ rvo/rvo.cpp
@@ -7,12 +7,18 @@ using namespace std;
 
 bb_str foo()
 {
-    bb_str bb0 ("pattern 0");
-    return bb0;
+    bb_str bb ("~~~~~~~");
+    return bb;
 }
 
 int main(int argc, char* argv[])
 {
-    bb_str bb = foo();
-    bb.print_info();
+    cout << "NRVO in action:"<<endl;
+    bb_str bb0 = foo();
+    bb0.print_info();
+    cout << string('-', 80)<<endl<<endl<<endl;
+
+    cout << "move semantic in action:"<<endl;
+    bb_str bb1("*********");
+    bb1 = foo();
 }
